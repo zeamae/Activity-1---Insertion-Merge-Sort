9bd454f646561995ff96652ff8b353963ab87079
zeamae <60580288+zeamae@users.noreply.github.com>
2020-02-24 02:32:13 +0800

    Update final_INSERTION_Sort.py

--- final_INSERTION_Sort.py
+++ final_INSERTION_Sort.py
@@ -1,140 +1,143 @@
-#FINAL INSERTION SORT
+#FINAL INSERTION SORT!!!
 
 import time
 import random
 import matplotlib.pyplot as plt
 
-start_time = time.time()
+
 x = []
 y = []
 
+print("INSERTION SORT")
+print("--To be sorted--")
+
+z1 = int(input("No. of integers: "))
+start_time = time.time()
+ins1 = random.sample(range(0, z1), z1)
 
 def insertion_sort1():
-    z = 1000
-    numGen = random.sample(range(0, z), z)
-
-    for i in range(1, z):
-        value = numGen[i]
+    for i in range(0, z1):
+        value = ins1[i]
         pos = i
-        while pos > 0 and numGen[pos - 1] > value:
-            numGen[pos] = numGen[pos - 1]
+        while pos > 0 and ins1[pos - 1] > value:
+            ins1[pos] = ins1[pos - 1]
             pos = pos - 1
 
-        numGen[pos] = value
+        ins1[pos] = value
 
-    print(numGen)
-    x.append(z)
+    print(ins1)
+    x.append(z1)
 
-    end_time = time.time()
-    runTime = end_time - start_time
-    print("Execution time:", runTime, "seconds")
-    y.append(runTime)
+end_time = time.time()
+runTime1 = end_time - start_time
+print("Execution time:", runTime1)
 
 insertion_sort1()
-print("")
+
+z2 = int(input("No. of integers: "))
+start_time = time.time()
+ins2 = random.sample(range(0, z2), z2)
 
 def insertion_sort2():
-    z = 500
-    numGen = random.sample(range(0, z), z)
-
-    for i in range(1, z):
-        value = numGen[i]
+    for i in range(1, z2):
+        value = ins2[i]
         pos = i
-        while pos > 0 and numGen[pos - 1] > value:
-            numGen[pos] = numGen[pos - 1]
+        while pos > 0 and ins2[pos - 1] > value:
+            ins2[pos] = ins2[pos - 1]
             pos = pos - 1
 
-        numGen[pos] = value
+        ins2[pos] = value
 
-    print(numGen)
-    x.append(z)
+    print(ins2)
+    x.append(z2)
 
-    end_time = time.time()
-    runTime = end_time - start_time
-    print("Execution time:", runTime, "seconds")
-    y.append(runTime)
+end_time = time.time()
+runTime2 = end_time - start_time
+print("Execution time:", runTime2)
 
 insertion_sort2()
-print("")
+
+z3 = int(input("No. of integers: "))
+start_time = time.time()
+ins3 = random.sample(range(0, z3), z3)
 
 def insertion_sort3():
-    z = 100
-    numGen = random.sample(range(0, z), z)
-
-    for i in range(1, z):
-        value = numGen[i]
+    for i in range(1, z3):
+        value = ins3[i]
         pos = i
-        while pos > 0 and numGen[pos - 1] > value:
-            numGen[pos] = numGen[pos - 1]
+        while pos > 0 and ins3[pos - 1] > value:
+            ins3[pos] = ins3[pos - 1]
             pos = pos - 1
 
-        numGen[pos] = value
+        ins3[pos] = value
 
-    print(numGen)
-    x.append(z)
+    print(ins3)
+    x.append(z3)
 
-    end_time = time.time()
-    runTime = end_time - start_time
-    print("Execution time:", runTime, "seconds")
-    y.append(runTime)
+end_time = time.time()
+runTime3 = end_time - start_time
+print("Execution time:", runTime3)
+
 
 insertion_sort3()
-print("")
+
+z4 = int(input("No. of integers: "))
+start_time = time.time()
+ins4 = random.sample(range(0, z4), z4)
 
 def insertion_sort4():
-    z = 1200
-    numGen = random.sample(range(0, z), z)
-
-    for i in range(1, z):
-        value = numGen[i]
+    for i in range(1, z4):
+        value = ins4[i]
         pos = i
-        while pos > 0 and numGen[pos - 1] > value:
-            numGen[pos] = numGen[pos - 1]
+        while pos > 0 and ins4[pos - 1] > value:
+            ins4[pos] = ins4[pos - 1]
             pos = pos - 1
 
-        numGen[pos] = value
+        ins4[pos] = value
 
-    print(numGen)
-    x.append(z)
+    print(ins4)
+    x.append(z4)
 
-    end_time = time.time()
-    runTime = end_time - start_time
-    print("Execution time:", runTime, "seconds")
-    y.append(runTime)
+end_time = time.time()
+runTime4 = end_time - start_time
+print("Execution time:", runTime4)
 
 insertion_sort4()
-print("")
+
+z5 = int(input("No. of integers: "))
+start_time = time.time()
+ins5 = random.sample(range(0, z5), z5)
 
 def insertion_sort5():
-    z = 1450
-    numGen = random.sample(range(0, z), z)
-
-    for i in range(1, z):
-        value = numGen[i]
+    for i in range(1, z5):
+        value = ins5[i]
         pos = i
-        while pos > 0 and numGen[pos - 1] > value:
-            numGen[pos] = numGen[pos - 1]
+        while pos > 0 and ins5[pos - 1] > value:
+            ins5[pos] = ins5[pos - 1]
             pos = pos - 1
 
-        numGen[pos] = value
+        ins5[pos] = value
 
-    print(numGen)
-    x.append(z)
+    print(ins5)
+    x.append(z5)
 
-    end_time = time.time()
-    runTime = end_time - start_time
-    print("Execution time:", runTime, "seconds")
-    y.append(runTime)
+end_time = time.time()
+runTime5 = end_time - start_time
+print("Execution time:", runTime5)
 
 insertion_sort5()
-print("")
 
 
-print(x)
+left = [1, 2, 3, 4, 5]
+height = [runTime1, runTime2, runTime3, runTime4, runTime5]
 
-plt.plot(x, y, 'bo')
+tick_label = [z1, z2, z3, z4, z5]
+
+plt.bar(left, height, tick_label=tick_label,
+        width=0.5, color=['green'])
+
 plt.xlabel('Number of Integers')
 plt.ylabel('Execution Time (sec)')
-plt.axis([0, 2000, 0, 0.50])
+
 plt.title("Insertion Sort")
 plt.show()
